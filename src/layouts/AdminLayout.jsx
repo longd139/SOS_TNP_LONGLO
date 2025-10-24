61c1f8f8b0671ca4e54681368d07cfb978806a68
Mai Thi Thanh Ngan <thanhngan.pt2004@gmail.com>
2025-10-24 11:49:14 +0700

    Implement layout for login feature (#3)

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 
-const AdminLayout = ({ children }) => {
+const AdminLayout = () => {
   return (
     <div className="flex min-h-screen bg-gray-50">
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header />
-        <main className="p-6 flex-1">{children}</main>
+        <main className="p-6 flex-1">
+          <Outlet />
+        </main>
       </div>
     </div>
   );
