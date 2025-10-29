18737667dfd656d26d4729127d62dd474e9cea96
nganne <thanhngan.pt2004@gmail.com>
2025-10-29 11:59:53 +0700

    fixing compile

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Auth/Login";
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <AppRoutes />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { HelmetProvider } from "react-helmet-async";
-import { BrowserRouter } from "react-router-dom"; 
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from './contexts/AuthContext';
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <HelmetProvider>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    </HelmetProvider>
 );
