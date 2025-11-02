bfbdc79c1c7a2ceeb51748ec9fd6bd59dd924739
Lê Nguyễn Minh Kha <lenguyenminhkha1606@gmail.com>
2025-11-02 09:40:14 +0700

    rerun

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { AuthProvider } from './contexts/AuthContext';
 import AppRoutes from './routes/AppRoutes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+// import './App.css';
 function App() {
     return (
         <AuthProvider>
