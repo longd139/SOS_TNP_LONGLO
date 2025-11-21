52d09446f42c5d0d5ccbf30a6d34e74988f23ac0
Vinh Timoon <vinhm281@gmail.com>
2025-11-21 18:50:16 +0700

    add label require

--- src/pages/Contact/ContactInfo.jsx
+++ src/pages/Contact/ContactInfo.jsx
@@ -358,7 +358,7 @@ export default function ContactInfo() {
                             )}
 
                             <div>
-                                <label className="block text-sm font-medium text-gray-600 mb-1">
+                                <label className="block text-sm font-medium text-gray-600 mb-1 required-label">
                                     Ghi chú
                                 </label>
                                 <input
