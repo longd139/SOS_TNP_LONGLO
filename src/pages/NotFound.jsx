57e3c97fc5787d17619a55f5446ad522db6f647a
dhgb2k5 <dhgb2k5@gmail.com>
2025-12-11 23:08:48 +0700

    fix: 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
 export default function NotFound() {
+  const [countdown, setCountdown] = useState(5);
+  
+  useEffect(() => {
+    if (countdown === 0) {
+      window.location.href = '/';
+      return;
+    }
+
+  const countdownFunc = setInterval(() => {
+    setCountdown((prev) => prev - 1);
+  }, 1000)
+
+  return () => clearInterval(countdownFunc);
+
+  }, [countdown]);
+
   return (
-    <div>
-      <h2>404 - Not Found</h2>
-      <p>Trang bạn tìm kiếm không tồn tại.</p>
+    <div className="text-center mt-20">
+      <h2 className="md:text-5xl text-2xl font-bold">404 - Not Found</h2>
+      <p className="md:text-xl text-xl">Trang bạn tìm kiếm không tồn tại.</p>
+      <p className="text-gray-500">Tự động quay về trang chủ sau <span>{countdown}</span> giây</p>
+      <button className="mt-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700" onClick={() => window.location.href='/'}>
+        Quay về trang chủ
+      </button>
     </div>
   )
 }
