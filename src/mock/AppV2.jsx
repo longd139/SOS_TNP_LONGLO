4da6d59672e603c38ecc2149b5ae39b46c5f93e8
FPTxNDT <nthanh240205@gmail.com>
2026-08-06 14:48:38 +0700

    fix: boc AppV2 trong Redux Provider

--- src/mock/AppV2.jsx
+++ src/mock/AppV2.jsx
@@ -4,18 +4,22 @@
 // ============================================================
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from '../app/store';
 import { MockProvider } from './MockContext';
 import AppRoutesV2 from './AppRoutesV2';
 import { AuthProvider } from '../contexts/AuthContext';
 
 export default function AppV2() {
   return (
-    <AuthProvider>
-      <MockProvider>
-        <BrowserRouter>
-          <AppRoutesV2 />
-        </BrowserRouter>
-      </MockProvider>
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <MockProvider>
+          <BrowserRouter>
+            <AppRoutesV2 />
+          </BrowserRouter>
+        </MockProvider>
+      </AuthProvider>
+    </Provider>
   );
 }
